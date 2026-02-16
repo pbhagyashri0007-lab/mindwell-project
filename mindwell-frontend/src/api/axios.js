ad05721ce7cccc9e1ae0b50ff16b424b2175a7d4
Bhagyashri Patil <pbhagyashri0007@gmail.com>
2026-02-16 11:13:06 +0530

    Connected frontend to live backend

--- mindwell-frontend/src/api/axios.js
+++ mindwell-frontend/src/api/axios.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mindwell-project-caob.onrender.com"
-});
-
-// Add token automatically to every request
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
+  baseURL: "https://mindwell-project-caob.onrender.com/api",
 });
 
 export default API;
